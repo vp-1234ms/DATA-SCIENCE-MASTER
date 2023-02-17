ec19a3913a8e27cebfc078c58c605f53e24e48f7
Vaibhav Pawar <102847008+vp-1234ms@users.noreply.github.com>
2023-02-17 18:23:18 +0530

    Add files via upload

---
+++ DAY16.1.py
@@ -0,0 +1,13 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="abc",
+  password="password"
+)
+print(mydb)
+mycursor = mydb.cursor()
+mycursor.execute("SHOW DATABASES")
+for x in mycursor:
+  print(x)
+
+#Shows all databases exsisting in Myssql
